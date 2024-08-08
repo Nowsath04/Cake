74f3ed3f085bd48616b8141a8189cb1fd2476485
Nowsath04 <nowsathnichu@gmail.com>
2024-08-08 10:34:02 +0530

    added

--- src/components/WalletPageComponent/AccontDetails/AccontDetails.jsx
+++ src/components/WalletPageComponent/AccontDetails/AccontDetails.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./AccontDetails.css"
+import { useSelector } from 'react-redux'
 
 function AccontDetails() {
+
+
+    const { user } = useSelector((useSelector) => useSelector.auth)
+
+
+    useEffect(() => {
+        console.log(user);
+    }, [user])
+
     return (
-        <div>
-            AccontDetails
+        <div className='General_div'>
+            <div className='General_heading'>Account Details</div>
+            <div className='General_content_box_div'>
+                <div className='General_content_box' style={{ width: "100%" }}>
+                    <div className='General_content_box_heading'>Wallet Address</div>
+                    <div className='General_content_box_subheading'><span>{user?.userid}</span></div>
+                </div>
+            </div>
         </div>
     )
 }
